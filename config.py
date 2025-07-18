f0d3cbedc8519f031507e7931220d183db14526f
UsmanT12 <144747599+UsmanT12@users.noreply.github.com>
2025-07-17 21:59:16 -0700

    Delete config.py

--- config.py
+++
@@ -1,3 +0,0 @@
-# Configuration file for storing paths and other settings
-CHROME_DRIVER_PATH = '/Users/usmantahir/Github_Clones/chromedriver'
-SCHEDULE_URL = 'https://www.wnba.com/schedule?season=2024&month=all'
